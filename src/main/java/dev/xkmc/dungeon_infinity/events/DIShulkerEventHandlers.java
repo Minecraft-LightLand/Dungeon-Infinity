96112237e479d44e1b10828aaa261ea5aca964c6
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-06-28 21:39:39 +0800

    text color fix

--- src/main/java/dev/xkmc/dungeon_infinity/events/DIShulkerEventHandlers.java
+++ src/main/java/dev/xkmc/dungeon_infinity/events/DIShulkerEventHandlers.java
@@ -2,6 +2,7 @@ package dev.xkmc.dungeon_infinity.events;
 
 import dev.xkmc.dungeon_infinity.init.DungeonInfinity;
 import dev.xkmc.l2menustacker.init.L2MSLangData;
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.network.chat.contents.TranslatableContents;
 import net.minecraft.tags.ItemTags;
@@ -21,7 +22,7 @@ public class DIShulkerEventHandlers {
 				event.getToolTip().removeIf(e -> e.getContents() instanceof TranslatableContents tr &&
 						(tr.getKey().equals("item.container.item_count") || tr.getKey().equals("item.container.more_items")));
 			}
-			event.getToolTip().add(L2MSLangData.QUICK_ACCESS.get());
+			event.getToolTip().add(L2MSLangData.QUICK_ACCESS.get().withStyle(ChatFormatting.GRAY));
 		}
 	}
 
