f3ba586d3d0d2ca6c6d2ef914bd1c87b1ee1c062
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-06-29 14:56:30 +0800

    page button

--- src/main/java/dev/xkmc/dungeon_infinity/content/map/MazeMapScreen.java
+++ src/main/java/dev/xkmc/dungeon_infinity/content/map/MazeMapScreen.java
@@ -26,6 +26,8 @@ public class MazeMapScreen extends Screen {
 
 	private final long seed;
 
+	private int layerY, diffY;
+
 	private int btnUpX, btnUpY, btnDownX, btnDownY;
 
 	protected MazeMapScreen(long seed) {
@@ -37,7 +39,7 @@ public class MazeMapScreen extends Screen {
 	public boolean mouseClicked(MouseButtonEvent event, boolean doubleClick) {
 		int mx = (int) event.x();
 		int my = (int) event.y();
-		int x0 = width/2, y0 = height/2;
+		int x0 = width / 2, y0 = height / 2;
 		float rate = Math.min(x0 / 64f, y0 / 64f) / 1.5f;
 		int cmx = (int) (((mx - x0) / rate + 63) / 5);
 		int cmz = (int) (((my - y0) / rate + 63) / 5);
@@ -50,16 +52,24 @@ public class MazeMapScreen extends Screen {
 				int z = wp % 400;
 				if (x / 16 == cmx && z / 16 == cmz) {
 					DungeonInfinity.HANDLER.toServer(new UseWaypointPacket(pos.at(x, z)));
+					diffY = 0;
 					return true;
 				}
 			}
-		}
-		var font = getFont();
-		if (mx >= btnUpX && mx <= btnUpX + font.width("↑") && my >= btnUpY && my <= btnUpY + font.lineHeight) {
-			return true;
-		}
-		if (mx >= btnDownX && mx <= btnDownX + font.width("↓") && my >= btnDownY && my <= btnDownY + font.lineHeight) {
-			return true;
+			var font = getFont();
+			layerY = pos.y();
+			if (mx >= btnUpX && mx <= btnUpX + font.width("↑") && my >= btnUpY && my <= btnUpY + font.lineHeight) {
+				if (Mth.clamp(layerY + diffY + 1, 0, 15) != diffY + layerY) {
+					diffY++;
+					return true;
+				}
+			}
+			if (mx >= btnDownX && mx <= btnDownX + font.width("↓") && my >= btnDownY && my <= btnDownY + font.lineHeight) {
+				if (Mth.clamp(layerY + diffY - 1, 0, 15) != diffY + layerY) {
+					diffY--;
+					return true;
+				}
+			}
 		}
 		return super.mouseClicked(event, doubleClick);
 	}
@@ -70,6 +80,10 @@ public class MazeMapScreen extends Screen {
 		Player player = Minecraft.getInstance().player;
 		if (player == null) return;
 		var pos = MazePos.map(player.blockPosition());
+		layerY = pos.y();
+		var mpy = Mth.clamp(layerY + diffY, 0, 15);
+		diffY = mpy - layerY;
+		pos = pos.atLayer(mpy);
 		var tex = MazeMapTextureManager.get().getDetail(seed, pos);
 		var fog = MazeMapTextureManager.get().getFog(seed, pos);
 		var visit = DIMeta.HISTORY.type().getOrCreate(player).getOrCreate(pos);
@@ -90,30 +104,30 @@ public class MazeMapScreen extends Screen {
 			g.blit(RenderPipelines.GUI_TEXTURED, fog.id, 0, 0, 0, 0, 25, 25, 32, 32);
 		g.pose().popMatrix();
 
-		g.pose().pushMatrix();
-		g.pose().translate(pos.px() / 16f * 5f, pos.pz() / 16f * 5f);
-		g.pose().scale(2, 2);
-		var yrot = player.getYRot();
-		g.pose().rotate(yrot * Mth.DEG_TO_RAD);
-
-		float r = (Mth.sin(((int) (System.currentTimeMillis() % 2000)) / 2000f * Math.PI * 2) + 1) / 2;
-		float pulse = 1 + r * 0.2f;
-		g.pose().scale(pulse, pulse);
-		int col = 0xffffffff;
-
-		g.pose().pushMatrix();
-		g.pose().scale(1.5f, 1.5f);
-		g.submitGuiElementRenderState(Arrow.of(g, 1, 0xff000000));
-		g.pose().popMatrix();
-		g.submitGuiElementRenderState(Arrow.of(g, 1, col));
-		g.pose().popMatrix();
+		if (diffY == 0) {
+			g.pose().pushMatrix();
+			g.pose().translate(pos.px() / 16f * 5f, pos.pz() / 16f * 5f);
+			g.pose().scale(2, 2);
+			var yrot = player.getYRot();
+			g.pose().rotate(yrot * Mth.DEG_TO_RAD);
+			float r = (Mth.sin(((int) (System.currentTimeMillis() % 2000)) / 2000f * Math.PI * 2) + 1) / 2;
+			float pulse = 1 + r * 0.2f;
+			g.pose().scale(pulse, pulse);
+			int col = 0xffffffff;
+			g.pose().pushMatrix();
+			g.pose().scale(1.5f, 1.5f);
+			g.submitGuiElementRenderState(Arrow.of(g, 1, 0xff000000));
+			g.pose().popMatrix();
+			g.submitGuiElementRenderState(Arrow.of(g, 1, col));
+			g.pose().popMatrix();
+		}
 
 		for (int wp : visit.getAllWaypoints()) {
 			int x = wp / 400 % 400;
 			int z = wp % 400;
 			x = x & -0xF | 0x8;
 			z = z & -0xF | 0x8;
-			col = MazeMapColors.P;
+			int col = MazeMapColors.P;
 			g.pose().pushMatrix();
 			g.pose().translate(x / 16f * 5f, z / 16f * 5f);
 			if (x / 16 == cmx && z / 16 == cmz) {
@@ -129,7 +143,7 @@ public class MazeMapScreen extends Screen {
 		var font = getFont();
 		int h = font.lineHeight + 3;
 
-		int bx = x1 + 40;
+		int bx = x1 + 50;
 		int by = y1;
 		btnUpX = bx;
 		btnUpY = by;
