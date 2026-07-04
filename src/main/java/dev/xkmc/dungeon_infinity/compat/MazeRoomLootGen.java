1ebac687a0ce05866dc896d6d6945a9900b4e084
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-07-04 20:52:17 +0800

    regen reward

--- src/main/java/dev/xkmc/dungeon_infinity/compat/MazeRoomLootGen.java
+++ src/main/java/dev/xkmc/dungeon_infinity/compat/MazeRoomLootGen.java
@@ -8,9 +8,11 @@ import dev.xkmc.modulargolems.init.registrate.GolemItems;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.storage.loot.LootPool;
 import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.entries.LootItem;
+import net.minecraft.world.level.storage.loot.entries.LootPoolSingletonContainer;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.world.level.storage.loot.predicates.LootItemRandomChanceCondition;
 
@@ -24,6 +26,7 @@ public class MazeRoomLootGen {
 	private static ResourceKey<LootTable> maze(String path) {
 		return loot("maze/" + path);
 	}
+
 	// ===== STONE 石制级 =====
 	public static final ResourceKey<LootTable> STONE_ROOM_BASIC = maze("stone/room/basic");
 	public static final ResourceKey<LootTable> STONE_ROOM_LARGE = maze("stone/room/large");
@@ -69,6 +72,18 @@ public class MazeRoomLootGen {
 	public static final ResourceKey<LootTable> SCULK_QUAD = maze("sculk/quad");
 	public static final ResourceKey<LootTable> SCULK_BOSS = maze("sculk/boss");
 
+	private static LootPoolSingletonContainer.Builder<?> byChance(ItemLike item, float chance) {
+		return LootItem.lootTableItem(item).when(LootTableTemplate.chance(chance));
+	}
+
+	private static LootPool.Builder single(ItemLike item, int min, int max) {
+		return LootPool.lootPool().add(LootTableTemplate.getItem(item.asItem(), min, max));
+	}
+
+	private static LootPoolSingletonContainer.Builder<?> weight(ItemLike item, int weight) {
+		return LootItem.lootTableItem(item).setWeight(weight);
+	}
+
 	public static void genLoot(RegistrateLootTableProvider pvd) {
 
 		// ==================== STONE ====================
@@ -77,181 +92,150 @@ public class MazeRoomLootGen {
 		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(STONE_ROOM_BASIC,
 				LootTable.lootTable()
 						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.GOLD_INGOT, 1, 3))
-								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 2, 5))
+								.add(LootTableTemplate.getItem(Items.GOLD_INGOT, 1))
+								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 1, 3))
 						)
+						.withPool(single(Items.BREAD, 1, 3))
+						.withPool(single(Items.OAK_LOG, 2, 4))
 						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.BREAD, 1, 3))
+								.when(LootItemRandomChanceCondition.randomChance(1 / 16f))
+								.add(weight(GolemItems.QUARTZ.get(), 1))
 						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.OAK_LOG, 2, 5))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootItem.lootTableItem(GolemItems.QUARTZ.get()))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EXPERIENCE_BOTTLE, 2, 5))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EMERALD, 1, 3))
-						)
+						.withPool(single(Items.EXPERIENCE_BOTTLE, 2, 3))
+						.withPool(single(Items.EMERALD, 1, 2))
 		));
 
 		// LARGE: 大型兵掉落，金属块+钻石+护甲材料
 		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(STONE_ROOM_LARGE,
 				LootTable.lootTable()
 						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.GOLD_INGOT, 2, 5))
-								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 3, 8))
-								.add(LootTableTemplate.getItem(Items.DIAMOND, 1, 2))
+								.add(LootTableTemplate.getItem(Items.GOLD_INGOT, 1, 2))
+								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 2, 3))
 						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.COOKED_PORKCHOP, 2, 4))
-						)
+						.withPool(single(Items.BAKED_POTATO, 2, 4))
 						.withPool(LootPool.lootPool()
 								.add(LootTableTemplate.getItem(Items.OAK_LOG, 3, 6))
-								.add(LootTableTemplate.getItem(Items.LEATHER, 1, 3))
+								.add(LootTableTemplate.getItem(Items.CLAY, 3, 6))
 						)
 						.withPool(LootPool.lootPool()
-								.add(LootItem.lootTableItem(GolemItems.GOLD.get()))
+								.when(LootItemRandomChanceCondition.randomChance(5 / 16f))
+								.add(weight(GolemItems.GOLD.get(), 1))
+								.add(weight(GolemItems.DIAMOND.get(), 1))
+								.add(weight(GolemItems.QUARTZ.get(), 3))
 						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EXPERIENCE_BOTTLE, 3, 6))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EMERALD, 1, 3))
-						)
+						.withPool(single(Items.EXPERIENCE_BOTTLE, 3, 5))
+						.withPool(single(Items.EMERALD, 1, 3))
 		));
 
 		// RANGED: 远程掉落，大量箭矢
 		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(STONE_ROOM_RANGED,
 				LootTable.lootTable()
 						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.GOLD_INGOT, 1, 3))
-								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 2, 5))
-								.add(LootTableTemplate.getItem(Items.ARROW, 6, 12))
+								.add(LootTableTemplate.getItem(Items.GOLD_INGOT, 1, 2))
+								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 2, 3))
+						)
+						.withPool(single(Items.BAKED_POTATO, 2, 4))
+						.withPool(LootPool.lootPool()
+								.add(LootTableTemplate.getItem(Items.OAK_LOG, 3, 6))
+								.add(LootTableTemplate.getItem(Items.CLAY, 3, 6))
 						)
 						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.BREAD, 1, 3))
-								.add(LootTableTemplate.getItem(Items.CARROT, 2, 4))
+								.when(LootItemRandomChanceCondition.randomChance(5 / 16f))
+								.add(weight(GolemItems.GOLD.get(), 1))
+								.add(weight(GolemItems.WEAK.get(), 1))
+								.add(weight(GolemItems.WITHER.get(), 1))
+								.add(weight(GolemItems.SLOW.get(), 1))
+								.add(weight(GolemItems.SPEED.get(), 1))
 						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.OAK_LOG, 2, 5))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootItem.lootTableItem(GolemItems.QUARTZ.get()))
-								.add(LootItem.lootTableItem(GolemItems.GOLD.get()))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EXPERIENCE_BOTTLE, 2, 5))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EMERALD, 1, 3))
-						)
+						.withPool(single(Items.ARROW, 12, 24))
+						.withPool(single(Items.EXPERIENCE_BOTTLE, 3, 5))
+						.withPool(single(Items.EMERALD, 1, 3))
 		));
 
 		// RIDER: 骑兵掉落，火药+皮革+金胡萝卜
 		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(STONE_ROOM_RIDER,
 				LootTable.lootTable()
 						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.GOLD_INGOT, 1, 3))
-								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 2, 5))
-								.add(LootTableTemplate.getItem(Items.GUNPOWDER, 2, 5))
+								.add(LootTableTemplate.getItem(Items.GOLD_INGOT, 1, 2))
+								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 2, 3))
+						)
+						.withPool(single(Items.GOLDEN_CARROT, 1, 2))
+						.withPool(LootPool.lootPool()
+								.add(LootTableTemplate.getItem(Items.OAK_LOG, 3, 6))
+								.add(LootTableTemplate.getItem(Items.CLAY, 3, 6))
 						)
 						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.GOLDEN_CARROT, 1, 2))
-								.add(LootTableTemplate.getItem(Items.CARROT, 2, 5))
+								.when(LootItemRandomChanceCondition.randomChance(5 / 16f))
+								.add(weight(GolemItems.GOLD.get(), 1))
+								.add(weight(GolemItems.MOUNT_UPGRADE.get(), 3))
+								.add(weight(GolemItems.SPEED.get(), 1))
 						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.OAK_LOG, 2, 5))
-								.add(LootTableTemplate.getItem(Items.LEATHER, 2, 4))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootItem.lootTableItem(GolemItems.QUARTZ.get()))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EXPERIENCE_BOTTLE, 2, 5))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EMERALD, 2, 4))
-						)
+						.withPool(single(Items.EXPERIENCE_BOTTLE, 3, 5))
+						.withPool(single(Items.EMERALD, 1, 3))
 		));
 
 		// MIXED: 混合掉落，各类型汇集+额外奖励
 		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(STONE_ROOM_MIXED,
 				LootTable.lootTable()
 						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.GOLD_INGOT, 2, 5))
-								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 3, 8))
-								.add(LootTableTemplate.getItem(Items.DIAMOND, 1, 3))
+								.add(LootTableTemplate.getItem(Items.GOLD_INGOT, 1, 3))
+								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 3, 5))
+						)
+						.withPool(single(Items.COOKED_BEEF, 3, 5))
+						.withPool(LootPool.lootPool()
+								.add(LootTableTemplate.getItem(Items.OAK_LOG, 3, 6))
+								.add(LootTableTemplate.getItem(Items.CLAY, 3, 6))
 						)
 						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.BREAD, 2, 4))
-								.add(LootTableTemplate.getItem(Items.GOLDEN_CARROT, 1, 2))
+								.when(LootItemRandomChanceCondition.randomChance(7 / 16f))
+								.add(weight(GolemItems.GOLD.get(), 2))
+								.add(weight(GolemItems.DIAMOND.get(), 2))
+								.add(weight(GolemItems.SPEED.get(), 2))
+								.add(weight(GolemItems.SIZE_UPGRADE.get(), 1))
 						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.ARROW, 4, 10))
-								.add(LootTableTemplate.getItem(Items.GUNPOWDER, 1, 4))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.OAK_LOG, 3, 8))
-								.add(LootTableTemplate.getItem(Items.LEATHER, 1, 3))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootItem.lootTableItem(GolemItems.QUARTZ.get()))
-								.add(LootItem.lootTableItem(GolemItems.GOLD.get()))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EXPERIENCE_BOTTLE, 4, 8))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EMERALD, 2, 5))
-						)
+						.withPool(single(Items.EXPERIENCE_BOTTLE, 4, 6))
+						.withPool(single(Items.EMERALD, 2, 4))
 		));
 
 		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(STONE_QUAD,
 				LootTable.lootTable()
 						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.GOLD_INGOT, 4, 10))
-								.add(LootTableTemplate.getItem(Items.DIAMOND, 3, 6))
-								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 4, 10))
+								.add(LootTableTemplate.getItem(Items.GOLD_INGOT, 2, 4))
+								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 4, 6))
+						)
+						.withPool(single(Items.COOKED_BEEF, 3, 5))
+						.withPool(LootPool.lootPool()
+								.add(LootTableTemplate.getItem(Items.OAK_LOG, 3, 6))
+								.add(LootTableTemplate.getItem(Items.CLAY, 3, 6))
 						)
 						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.COOKED_PORKCHOP, 2, 4))
-								.add(LootTableTemplate.getItem(Items.BREAD, 3, 6))
+								.when(LootItemRandomChanceCondition.randomChance(7 / 16f))
+								.add(weight(GolemItems.GOLD.get(), 2))
+								.add(weight(GolemItems.DIAMOND.get(), 2))
+								.add(weight(GolemItems.PICKUP.get(), 3))
 						)
-						.withPool(LootPool.lootPool()
-								.add(LootItem.lootTableItem(GolemItems.SLOW.get()))
-								.add(LootItem.lootTableItem(GolemItems.PICKUP.get()))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EXPERIENCE_BOTTLE, 4, 8))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EMERALD, 6, 12))
-						)
+						.withPool(single(Items.EXPERIENCE_BOTTLE, 4, 6))
+						.withPool(single(Items.EMERALD, 4, 6))
 		));
 
-		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(STONE_BOSS,
+		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(MINESHAFT_BOSS,
 				LootTable.lootTable()
 						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.GOLD_INGOT, 6, 14))
-								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 6, 12))
+								.add(LootTableTemplate.getItem(Items.GOLD_INGOT, 4, 6))
+								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 6, 10))
 						)
+						.withPool(single(Items.DIAMOND, 2, 3))
+						.withPool(single(Items.COOKED_BEEF, 3, 5))
+						.withPool(single(Items.LAPIS_LAZULI, 3, 5))
 						.withPool(LootPool.lootPool()
-								.add(LootItem.lootTableItem(Items.GOLDEN_APPLE))
-								.add(LootTableTemplate.getItem(Items.COOKED_PORKCHOP, 3, 6))
+								.add(weight(GolemItems.RECYCLE.get(), 1))
+								.add(weight(GolemItems.BELL.get(), 1))
+								.add(weight(GolemItems.GOLD.get(), 1))
+								.add(weight(GolemItems.DIAMOND.get(), 1))
+								.add(weight(GolemItems.PICKUP.get(), 1))
 						)
-						.withPool(LootPool.lootPool()
-								.add(LootItem.lootTableItem(GolemItems.ENCHANTED_GOLD.get()))
-								.add(LootItem.lootTableItem(GolemItems.QUARTZ.get()))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EXPERIENCE_BOTTLE, 5, 10))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EMERALD, 6, 12))
-						)
+						.withPool(single(Items.EXPERIENCE_BOTTLE, 4, 6))
+						.withPool(single(Items.EMERALD, 6, 10))
 		));
 
 		// ==================== MINESHAFT ====================
@@ -423,7 +407,7 @@ public class MazeRoomLootGen {
 						)
 		));
 
-		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(MINESHAFT_BOSS,
+		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(COPPER_BOSS,
 				LootTable.lootTable()
 						.withPool(LootPool.lootPool()
 								.add(LootTableTemplate.getItem(Items.IRON_INGOT, 10, 20))
@@ -642,7 +626,7 @@ public class MazeRoomLootGen {
 						)
 		));
 
-		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(COPPER_BOSS,
+		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(DEEPSLATE_BOSS,
 				LootTable.lootTable()
 						.withPool(LootPool.lootPool()
 								.add(LootTableTemplate.getItem(Items.DIAMOND, 6, 12))
@@ -858,7 +842,7 @@ public class MazeRoomLootGen {
 						)
 		));
 
-		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(DEEPSLATE_BOSS,
+		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(SCULK_BOSS,
 				LootTable.lootTable()
 						.withPool(LootPool.lootPool()
 								.add(LootTableTemplate.getItem(Items.DIAMOND, 6, 12))
@@ -1059,39 +1043,6 @@ public class MazeRoomLootGen {
 						)
 		));
 
-		pvd.addLootAction(LootContextParamSets.CHEST, sub -> sub.accept(SCULK_BOSS,
-				LootTable.lootTable()
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.ECHO_SHARD, 8, 16))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.NETHERITE_INGOT, 1, 2))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.ENCHANTED_GOLDEN_APPLE, 2, 4))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootItem.lootTableItem(GDItems.FLAME_SWORD.get()))
-								.add(LootItem.lootTableItem(GolemItems.NETHERITE.get()))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootItem.lootTableItem(GolemItems.RECYCLE.get()))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.NETHERITE_UPGRADE_SMITHING_TEMPLATE, 2, 4))
-								.add(LootTableTemplate.getItem(Items.ENCHANTED_GOLDEN_APPLE, 2, 3))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootItem.lootTableItem(GolemItems.ADD_NETHERITE.get()))
-								.add(LootItem.lootTableItem(GolemItems.TALENTED.get()))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EXPERIENCE_BOTTLE, 16, 24))
-						)
-						.withPool(LootPool.lootPool()
-								.add(LootTableTemplate.getItem(Items.EMERALD, 10, 20))
-						)
-		));
 	}
 
 }
