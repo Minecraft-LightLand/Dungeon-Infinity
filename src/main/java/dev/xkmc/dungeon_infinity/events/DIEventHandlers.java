d4d8ebfdada39afc0f1c0610177aa29e78ce32f8
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-07-09 22:15:48 +0800

    ver 28

--- src/main/java/dev/xkmc/dungeon_infinity/events/DIEventHandlers.java
+++ src/main/java/dev/xkmc/dungeon_infinity/events/DIEventHandlers.java
@@ -2,10 +2,9 @@ package dev.xkmc.dungeon_infinity.events;
 
 import dev.xkmc.dungeon_infinity.content.cap.MazeHistory;
 import dev.xkmc.dungeon_infinity.init.DungeonInfinity;
-import dev.xkmc.dungeon_infinity.init.reg.DIItems;
 import dev.xkmc.dungeon_infinity.init.data.DITagGen;
+import dev.xkmc.dungeon_infinity.init.reg.DIItems;
 import dev.xkmc.dungeon_infinity.init.reg.DIMeta;
-import dev.xkmc.modulargolems.init.data.MGTagGen;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.TriState;
 import net.minecraft.world.entity.LivingEntity;
@@ -13,7 +12,6 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.Blocks;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
@@ -47,9 +45,11 @@ public class DIEventHandlers {
 
 	public static boolean canUseOn(ItemStack stack, BlockState state) {
 		if (stack.is(DITagGen.ALWAYS_KEEP)) return true;
-		if (stack.is(Items.GLOWSTONE) && state.getBlock() == Blocks.RESPAWN_ANCHOR) return true;
+		if (state.getBlock() == Blocks.RESPAWN_ANCHOR)
+			return stack.isEmpty() || stack.is(DIItems.MAP) || stack.is(Items.GLOWSTONE);
 		return false;
 	}
+
 	@SubscribeEvent
 	public static void onItemUse(PlayerInteractEvent.RightClickBlock event) {
 		if (event.getItemStack().is(DIItems.MAP) &&
