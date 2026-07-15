4f27cfe52854294af7eca6fe8dcafecf90ff2c27
Dr-WeiAL <931311372@qq.com>
2026-07-15 22:26:19 +0800

    界面更新

--- src/main/java/dev/xkmc/dungeon_infinity/content/screen/MazeMapScreen.java
+++ src/main/java/dev/xkmc/dungeon_infinity/content/screen/MazeMapScreen.java
@@ -31,22 +31,39 @@ public class MazeMapScreen extends Screen implements MapUI {
 
 	private int layerY, diffY;
 
-	private final TextButton depthLabel, up, down, chorus, findQuad, findStair, findWarehouse, findWorkshop, findShop, configBtn;
+	private final TextButton depthLabel, up, down, chorus;
+	private final TextButton[] findBtns;
+	private final TextButton configBtn;
+
+	private record RoomTypeInfo(Component name, int color, int finderIndex) {}
+
+	private static final RoomTypeInfo[] ROOM_TYPES = {
+			new RoomTypeInfo(DILang.BATTLE.get(), MazeMapColors.F, -1),
+			new RoomTypeInfo(DILang.QUAD.get(), MazeMapColors.Q, 0),
+			new RoomTypeInfo(DILang.BOSS.get(), MazeMapColors.R, -1),
+			new RoomTypeInfo(DILang.DOWN_STAIR.get(), MazeMapColors.G, 1),
+			new RoomTypeInfo(DILang.UP_STAIR.get(), MazeMapColors.Y, -1),
+			new RoomTypeInfo(DILang.WORKSHOP.get(), MazeMapColors.K, 2),
+			new RoomTypeInfo(DILang.SHOP.get(), MazeMapColors.S, 3),
+			new RoomTypeInfo(DILang.WAREHOUSE.get(), MazeMapColors.H, 4),
+	};
 
 	public MazeMapScreen(long seed, boolean canUseWaypoint) {
 		super(Component.literal("Maze Map"));
 		this.seed = seed;
 		this.canUseWaypoint = canUseWaypoint;
 		depthLabel = new TextButton();
-		up = new TextButton();
-		down = new TextButton();
-		findQuad = new TextButton();
-		findStair = new TextButton();
-		findWarehouse = new TextButton();
-		findWorkshop = new TextButton();
-		findShop = new TextButton();
+		up = new TextButton().pad(4, 1).text(0xFFFFFFFF, 0xFFFFAA00, 0xFF606060);
+		down = new TextButton().pad(4, 1).text(0xFFFFFFFF, 0xFFFFAA00, 0xFF606060);
+		findBtns = new TextButton[]{
+				new TextButton().pad(4, 1),
+				new TextButton().pad(4, 1),
+				new TextButton().pad(4, 1),
+				new TextButton().pad(4, 1),
+				new TextButton().pad(4, 1),
+		};
 		chorus = new TextButton();
-		configBtn = new TextButton();
+		configBtn = new TextButton().pad(6, 3);
 	}
 
 	@Override
@@ -94,24 +111,24 @@ public class MazeMapScreen extends Screen implements MapUI {
 				DungeonInfinity.HANDLER.toServer(new UseSkillToServer(AllBuffs.CHORUS.id));
 				return true;
 			}
-			if (findQuad.contains(mx, my)) {
+			if (findBtns[0].contains(mx, my)) {
 				DungeonInfinity.HANDLER.toServer(new UseFinderToServer(RoomFinder.Type.QUAD));
 				return true;
 			}
-			if (findStair.contains(mx, my)) {
+			if (findBtns[1].contains(mx, my)) {
 				DungeonInfinity.HANDLER.toServer(new UseFinderToServer(RoomFinder.Type.STAIR));
 				return true;
 			}
-			if (findWarehouse.contains(mx, my)) {
-				DungeonInfinity.HANDLER.toServer(new UseFinderToServer(RoomFinder.Type.WAREHOUSE));
-				return true;
-			}
-			if (findWorkshop.contains(mx, my)) {
+			if (findBtns[2].contains(mx, my)) {
 				DungeonInfinity.HANDLER.toServer(new UseFinderToServer(RoomFinder.Type.WORKSHOP));
 				return true;
 			}
-			if (findShop.contains(mx, my)) {
+			if (findBtns[3].contains(mx, my)) {
 				DungeonInfinity.HANDLER.toServer(new UseFinderToServer(RoomFinder.Type.SHOP));
+				return true;
+			}
+			if (findBtns[4].contains(mx, my)) {
+				DungeonInfinity.HANDLER.toServer(new UseFinderToServer(RoomFinder.Type.WAREHOUSE));
 				return true;
 			}
 			if (configBtn.contains(mx, my)) {
@@ -155,19 +172,10 @@ public class MazeMapScreen extends Screen implements MapUI {
 		extractBuff(player, g, x0, y0, rate);
 
 		Component configText = DILang.MAP_SETTINGS.get();
-		int cfgW = font.width(configText);
-		int cfgBtnPX = 6;
-		int cfgBtnPY = 3;
+		int cfgW = configBtn.visualWidth(font, configText);
 		int cfgX = (g.guiWidth() - cfgW) / 2;
 		int cfgY = mapBottom + 9;
-		configBtn.set(cfgX - cfgBtnPX, cfgY - cfgBtnPY, cfgW + cfgBtnPX * 2, font.lineHeight + cfgBtnPY * 2);
-		boolean cfgH = configBtn.contains(mx, my);
-		g.fill(cfgX - cfgBtnPX, cfgY - cfgBtnPY, cfgX + cfgW + cfgBtnPX, cfgY + font.lineHeight + cfgBtnPY, cfgH ? 0xFF5A5A5A : 0xFF3D3D3D);
-		g.fill(cfgX - cfgBtnPX, cfgY - cfgBtnPY, cfgX + cfgW + cfgBtnPX, cfgY - cfgBtnPY + 1, cfgH ? 0xFF7A7A7A : 0xFF5A5A5A);
-		g.fill(cfgX - cfgBtnPX, cfgY + font.lineHeight + cfgBtnPY - 1, cfgX + cfgW + cfgBtnPX, cfgY + font.lineHeight + cfgBtnPY, 0xFF2A2A2A);
-		g.fill(cfgX - cfgBtnPX, cfgY - cfgBtnPY, cfgX - cfgBtnPX + 1, cfgY + font.lineHeight + cfgBtnPY, cfgH ? 0xFF7A7A7A : 0xFF5A5A5A);
-		g.fill(cfgX + cfgW + cfgBtnPX - 1, cfgY - cfgBtnPY, cfgX + cfgW + cfgBtnPX, cfgY + font.lineHeight + cfgBtnPY, 0xFF2A2A2A);
-		g.text(font, configText, cfgX, cfgY, cfgH ? 0xFFFFAA00 : 0xFFFFFFFF);
+		configBtn.update(g, true, cfgX, cfgY - 3, font, configText, mx, my);
 	}
 
 	public void extractLegend(Player player, GuiGraphicsExtractor g, int x0, int y0, float rate, MazePos pos) {
@@ -187,8 +195,6 @@ public class MazeMapScreen extends Screen implements MapUI {
 		g.fill(panelX, panelY, panelX + 1, panelY + panelH, 0xFF4A4A4A);
 		g.fill(panelX + panelW - 1, panelY, panelX + panelW, panelY + panelH, 0xFF2A2A2A);
 
-		int btnPadX = 4;
-		int btnPadY = 1;
 		int cx = panelX + 8;
 		int cy = panelY + 8;
 		int centerX = panelX + panelW / 2;
@@ -198,7 +204,7 @@ public class MazeMapScreen extends Screen implements MapUI {
 		g.pose().pushMatrix();
 		g.pose().translate(centerX, cy);
 		g.pose().scale(ts, ts);
-		g.text(font, infoTitle, -font.width(infoTitle) / 2, 0, -1, true);
+		g.text(font, infoTitle, -font.width(infoTitle) / 2, (int) (font.lineHeight * (1 - ts) / 2), -1, true);
 		g.pose().popMatrix();
 		cy += (int) (ts * h);
 		g.fill(cx, cy, panelX + panelW - 8, cy + 1, 0xFF4A4A4A);
@@ -213,97 +219,60 @@ public class MazeMapScreen extends Screen implements MapUI {
 		g.text(font, depthText, cx, cy, -1);
 
 		int maxDepthW = font.width(DILang.DEPTH.get(16));
-		depthLabel.set(cx - btnPadX, cy - btnPadY, maxDepthW + btnPadX * 2, lh + btnPadY * 2);
-		int btnY = cy - btnPadY;
+		depthLabel.set(cx - 4, cy - 1, maxDepthW + 8, lh + 2);
+		int btnY = cy - 1;
 		int rightEdge = panelX + panelW - 8;
 		Component upText = DILang.UP.get();
 		Component downText = DILang.DOWN.get();
 		int uw = font.width(upText);
 		int dw = font.width(downText);
-		int downX = rightEdge - dw - btnPadX;
+		int downX = rightEdge - dw - 4;
 		int upX = downX - uw - 9;
 
 		boolean upEnabled = pos.y() < 15;
-		up.set(upX - btnPadX, btnY, uw + btnPadX * 2, lh + btnPadY * 2);
-		boolean uh = upEnabled && up.contains(mx, my);
-		int upBg = upEnabled ? (uh ? 0xFF5A5A5A : 0xFF3D3D3D) : 0xFF2A2A2A;
-		int upBorder = upEnabled ? (uh ? 0xFF7A7A7A : 0xFF5A5A5A) : 0xFF3D3D3D;
-		g.fill(upX - btnPadX, btnY, upX + uw + btnPadX, btnY + lh + btnPadY * 2, upBg);
-		g.fill(upX - btnPadX, btnY, upX + uw + btnPadX, btnY + 1, upBorder);
-		g.fill(upX - btnPadX, btnY + lh + btnPadY * 2 - 1, upX + uw + btnPadX, btnY + lh + btnPadY * 2, 0xFF2A2A2A);
-		g.fill(upX - btnPadX, btnY, upX - btnPadX + 1, btnY + lh + btnPadY * 2, upBorder);
-		g.fill(upX + uw + btnPadX - 1, btnY, upX + uw + btnPadX, btnY + lh + btnPadY * 2, 0xFF2A2A2A);
-		g.text(font, upText, upX, cy, uh ? 0xFFFFAA00 : upEnabled ? 0xFFFFFFFF : 0xFF606060);
-		if (!upEnabled) up.disable();
+		up.update(g, upEnabled, upX - 4, btnY, font, upText, mx, my);
 
 		boolean downEnabled = pos.y() > 0;
-		down.set(downX - btnPadX, btnY, dw + btnPadX * 2, lh + btnPadY * 2);
-		boolean dh = downEnabled && down.contains(mx, my);
-		int downBg = downEnabled ? (dh ? 0xFF5A5A5A : 0xFF3D3D3D) : 0xFF2A2A2A;
-		int downBorder = downEnabled ? (dh ? 0xFF7A7A7A : 0xFF5A5A5A) : 0xFF3D3D3D;
-		g.fill(downX - btnPadX, btnY, downX + dw + btnPadX, btnY + lh + btnPadY * 2, downBg);
-		g.fill(downX - btnPadX, btnY, downX + dw + btnPadX, btnY + 1, downBorder);
-		g.fill(downX - btnPadX, btnY + lh + btnPadY * 2 - 1, downX + dw + btnPadX, btnY + lh + btnPadY * 2, 0xFF2A2A2A);
-		g.fill(downX - btnPadX, btnY, downX - btnPadX + 1, btnY + lh + btnPadY * 2, downBorder);
-		g.fill(downX + dw + btnPadX - 1, btnY, downX + dw + btnPadX, btnY + lh + btnPadY * 2, 0xFF2A2A2A);
-		g.text(font, downText, downX, cy, dh ? 0xFFFFAA00 : downEnabled ? 0xFFFFFFFF : 0xFF606060);
-		if (!downEnabled) down.disable();
+		down.update(g, downEnabled, downX - 4, btnY, font, downText, mx, my);
 
 		cy += h;
 		int finderCount = findable ? data.finder.finder : -1;
 		Component finderText = DILang.FINDER.get(Math.max(0, finderCount));
 		g.text(font, finderText, cx, cy, finderCount >= 0 ? -1 : 0xFF606060);
 		cy += h;
-		int chrousCount = data.buff.buffs.getOrDefault(AllBuffs.CHORUS.id, 0);
-		var chorusText = DILang.CHORUS.get(chrousCount);
+		int chorusCount = data.buff.buffs.getOrDefault(AllBuffs.CHORUS.id, 0);
+		var chorusText = DILang.CHORUS.get(chorusCount);
 		var visit = DIMeta.HISTORY.type().getOrCreate(player).getOrCreate(pos);
-		chorus.update(g, diffY == 0 && chrousCount > 0 && visit.getPath().length > 0, cx, cy, font, chorusText, mx, my);
+		float chorusScale = Math.min(1f, (panelW - 24) / (float) font.width(chorusText));
+		chorus.setScale(chorusScale);
+		chorus.update(g, diffY == 0 && chorusCount > 0 && visit.getPath().length > 0, cx, cy, font, chorusText, mx, my);
 
 		cy += h;
-
-
 		cy += 3;
 
 		renderFitText(g, DILang.ROOM_TYPES.get(), cx, cy, panelW - 16, 0xFFCCCCCC);
 		cy += h;
 
-		Component[] legendNames = {DILang.BATTLE.get(), DILang.QUAD.get(), DILang.BOSS.get(),
-				DILang.DOWN_STAIR.get(), DILang.UP_STAIR.get(), DILang.WORKSHOP.get(),
-				DILang.SHOP.get(), DILang.WAREHOUSE.get()};
-		int[] legendColors = {MazeMapColors.F, MazeMapColors.Q, MazeMapColors.R,
-				MazeMapColors.G, MazeMapColors.Y, MazeMapColors.K,
-				MazeMapColors.S, MazeMapColors.H};
-		int[] findOrder = {-1, 0, -1, 1, -1, 2, 3, 4};
-		TextButton[] findBtns = {findQuad, findStair, findWorkshop, findShop, findWarehouse};
-
 		float roomScale = 1f;
-		for (int idx = 0; idx < legendNames.length; idx++) {
-			boolean hasMag = findable && findOrder[idx] >= 0;
+		for (RoomTypeInfo info : ROOM_TYPES) {
+			boolean hasMag = findable && info.finderIndex >= 0;
 			float maxW = hasMag ? (panelW - 28 - mw) : (panelW - 16);
-			float s = Math.min(1f, maxW / font.width(legendNames[idx]));
+			float s = Math.min(1f, maxW / font.width(info.name));
 			if (s < roomScale) roomScale = s;
 		}
 
-		for (int i = 0; i < legendNames.length; i++) {
-			boolean hasMag = findable && findOrder[i] >= 0;
+		for (RoomTypeInfo info : ROOM_TYPES) {
+			boolean hasMag = findable && info.finderIndex >= 0;
 			int magX = hasMag ? panelX + panelW - 12 - mw : 0;
 			g.pose().pushMatrix();
 			g.pose().translate(cx, cy);
 			g.pose().scale(roomScale, roomScale);
-			g.text(font, legendNames[i], 0, 0, legendColors[i]);
+			g.text(font, info.name, 0, (int) (font.lineHeight * (1 - roomScale) / 2), info.color);
 			g.pose().popMatrix();
 			if (hasMag) {
-				int fi = findOrder[i];
-				TextButton btn = findBtns[fi];
-				btn.set(magX - btnPadX, cy - btnPadY, mw + btnPadX * 2, lh + btnPadY * 2);
-				boolean mh = btn.contains(mx, my);
-				g.fill(magX - btnPadX, cy - btnPadY, magX + mw + btnPadX, cy + lh + btnPadY, mh ? 0xFF5A5A5A : 0xFF3D3D3D);
-				g.fill(magX - btnPadX, cy - btnPadY, magX + mw + btnPadX, cy - btnPadY + 1, mh ? 0xFF7A7A7A : 0xFF5A5A5A);
-				g.fill(magX - btnPadX, cy + lh + btnPadY - 1, magX + mw + btnPadX, cy + lh + btnPadY, 0xFF2A2A2A);
-				g.fill(magX - btnPadX, cy - btnPadY, magX - btnPadX + 1, cy + lh + btnPadY, mh ? 0xFF7A7A7A : 0xFF5A5A5A);
-				g.fill(magX + mw + btnPadX - 1, cy - btnPadY, magX + mw + btnPadX, cy + lh + btnPadY, 0xFF2A2A2A);
-				g.text(font, mag, magX, cy, mh ? 0xFFFFAA00 : 0xFFFFFFFF);
-				if (mh) {
+				TextButton btn = findBtns[info.finderIndex];
+				btn.update(g, true, magX - 4, cy - 1, font, mag, mx, my);
+				if (btn.contains(mx, my)) {
 					g.setComponentTooltipForNextFrame(font, List.of(DILang.SEARCH.get()), mx, my);
 				}
 			}
@@ -337,7 +306,7 @@ public class MazeMapScreen extends Screen implements MapUI {
 		g.pose().pushMatrix();
 		g.pose().translate(centerX, cy);
 		g.pose().scale(ts, ts);
-		g.text(font, buffTitle, -font.width(buffTitle) / 2, 0, -1, true);
+		g.text(font, buffTitle, -font.width(buffTitle) / 2, (int) (font.lineHeight * (1 - ts) / 2), -1, true);
 		g.pose().popMatrix();
 		cy += (int) (ts * h);
 		g.fill(cx, cy, panelX + panelW - 8, cy + 1, 0xFF4A4A4A);
@@ -347,8 +316,8 @@ public class MazeMapScreen extends Screen implements MapUI {
 		for (var e : data.buffs.entrySet()) {
 			var buff = MazeBuff.get(e.getKey());
 			var title = buff.getTitle(e.getValue());
-			int w = font.width(title);
-			g.text(font, title, cx, cy, 0xFFFFAA00);
+			int w = Math.min(font.width(title), panelW - 16);
+			renderFitText(g, title, cx, cy, panelW - 16, 0xFFFFAA00);
 			if (cx <= mx && mx <= cx + w && cy <= my && my <= cy + h) {
 				g.setComponentTooltipForNextFrame(font, buff.getDetail(e.getValue()), mx, my);
 			}
@@ -362,7 +331,7 @@ public class MazeMapScreen extends Screen implements MapUI {
 		g.pose().pushMatrix();
 		g.pose().translate(x, y);
 		g.pose().scale(scale, scale);
-		g.text(font, text, 0, 0, color);
+		g.text(font, text, 0, (int) (font.lineHeight * (1 - scale) / 2), color);
 		g.pose().popMatrix();
 	}
 
--- src/main/java/dev/xkmc/dungeon_infinity/content/screen/TextButton.java
+++ src/main/java/dev/xkmc/dungeon_infinity/content/screen/TextButton.java
@@ -7,8 +7,63 @@ import net.minecraft.network.chat.Component;
 public class TextButton {
 
 	private boolean enabled;
-
 	private int x, y, w, h;
+
+	private int padX = 4, padY = 2;
+	private boolean fixed = false;
+	private int fixedW = 80;
+	private float scale = 1f;
+
+	private int bgNormal = 0xFF3D3D3D;
+	private int bgHover = 0xFF5A5A5A;
+	private int bgDisabled = 0xFF2A2A2A;
+	private int border = 0xFF5A5A5A;
+	private int borderHover = 0xFF7A7A7A;
+	private int borderDisabled = 0xFF3D3D3D;
+	private int borderBottom = 0xFF2A2A2A;
+	private int borderRight = 0xFF2A2A2A;
+	private int textNormal = 0xFFFFFFFF;
+	private int textHover = 0xFFFFAA00;
+	private int textDisabled = 0xFF808080;
+
+	public TextButton pad(int px, int py) {
+		padX = px; padY = py;
+		return this;
+	}
+
+	public TextButton fixedWidth(int w) {
+		fixed = true; fixedW = w;
+		return this;
+	}
+
+	public TextButton autoWidth() {
+		fixed = false;
+		return this;
+	}
+
+	public TextButton setScale(float scale) {
+		this.scale = scale;
+		return this;
+	}
+
+	public TextButton bg(int n, int h, int d) {
+		bgNormal = n; bgHover = h; bgDisabled = d;
+		return this;
+	}
+
+	public TextButton border(int n, int h, int d) {
+		border = n; borderHover = h; borderDisabled = d;
+		return this;
+	}
+
+	public TextButton text(int n, int h, int d) {
+		textNormal = n; textHover = h; textDisabled = d;
+		return this;
+	}
+
+	public int visualWidth(Font font, Component s) {
+		return fixed ? fixedW : (int) (font.width(s) * scale) + padX * 2;
+	}
 
 	public void set(int x, int y, int w, int h) {
 		this.x = x;
@@ -26,10 +81,42 @@ public class TextButton {
 		return enabled && mx >= x && my >= y && mx <= x + w && my <= y + h;
 	}
 
-	public void update(GuiGraphicsExtractor g, boolean enable, int x, int y, Font font, Component s, int mx, int my) {
-		if (enable) {
-			set(x, y, font.width(s), font.lineHeight);
-			g.text(font, s, x, y, contains(mx, my) ? 0xFFFFAA00 : 0xFFFFFFFF);
-		} else disable();
+	public void update(GuiGraphicsExtractor g, boolean enable, int bx, int by, Font font, Component s, int mx, int my) {
+		int tw = font.width(s);
+		int scaledTw = (int) (tw * scale);
+		int bw = fixed ? fixedW : scaledTw + padX * 2;
+		int bh = font.lineHeight + padY * 2;
+		set(bx, by, bw, bh);
+		int tx = bx + (bw - scaledTw) / 2;
+		int ty = by + padY + (int) ((font.lineHeight * (1 - scale)) / 2);
+		if (!enable) {
+			disable();
+			g.fill(bx, by, bx + bw, by + bh, bgDisabled);
+			g.fill(bx, by, bx + bw, by + 1, borderDisabled);
+			g.fill(bx, by + bh - 1, bx + bw, by + bh, borderBottom);
+			g.fill(bx, by, bx + 1, by + bh, borderDisabled);
+			g.fill(bx + bw - 1, by, bx + bw, by + bh, borderRight);
+			g.pose().pushMatrix();
+			g.pose().translate(tx, ty);
+			g.pose().scale(scale, scale);
+			g.text(font, s, 0, 0, textDisabled);
+			g.pose().popMatrix();
+			return;
+		}
+		boolean hover = contains(mx, my);
+		int bg = hover ? bgHover : bgNormal;
+		int tb = hover ? borderHover : border;
+		int lb = hover ? borderHover : border;
+		int tc = hover ? textHover : textNormal;
+		g.fill(bx, by, bx + bw, by + bh, bg);
+		g.fill(bx, by, bx + bw, by + 1, tb);
+		g.fill(bx, by + bh - 1, bx + bw, by + bh, borderBottom);
+		g.fill(bx, by, bx + 1, by + bh, lb);
+		g.fill(bx + bw - 1, by, bx + bw, by + bh, borderRight);
+		g.pose().pushMatrix();
+		g.pose().translate(tx, ty);
+		g.pose().scale(scale, scale);
+		g.text(font, s, 0, 0, tc);
+		g.pose().popMatrix();
 	}
 }
