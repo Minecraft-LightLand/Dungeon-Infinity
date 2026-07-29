75be9c210878256e68a82a2355d4ad3d827d563e
lcy0x1 <chengyilin@umail.ucsb.edu>
2026-07-29 14:50:33 +0800

    force field trigger position

--- src/main/java/dev/xkmc/dungeon_infinity/content/cap/SectionRoom.java
+++ src/main/java/dev/xkmc/dungeon_infinity/content/cap/SectionRoom.java
@@ -195,7 +195,8 @@ public class SectionRoom {
 				CellInterpreter.isQuadRoom(cell) ||
 				!CellInterpreter.isHallway(cell))) {
 			var origin = new Vec3(this.pos.origin());
-			var box = new AABB(origin.add(1, 1, 1), origin.add(15, 15, 15));
+			double r = 1.2; // TODO
+			var box = new AABB(origin.add(r, r, r), origin.add(16 - r, 16 - r, 16 - r));
 			if (box.contains(sp.position().add(sp.getBbHeight() / 2))) {
 				var ins = getOrCreateActiveMobRoomInstance();
 				ins.tick(sp);
